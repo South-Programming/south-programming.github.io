55a226d5113e6a19407b4900207603516c051dd1
ewaryang <edward.yang953@gmail.com>
2024-10-05 11:25:27 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js,css}"],
+  theme: {
+    fontFamily: {
+      'sans': [
+        'Inter',
+        'sans-serif'
+
+      ],
+      
+      'mono': [
+        'JetBrains Mono',
+        'monospace'
+      ]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
