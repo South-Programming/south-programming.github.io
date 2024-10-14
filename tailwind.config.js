c2bf4227fc6d8630264069fceb160dbdb139fc4d
omarabusharar <omarabusharar@users.noreply.github.com>
2024-10-14 06:28:28 +0000

    deploy: b318d66a49255be1f50b2df9da8f46b3f0088358

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js,css}", "./src/**/*.{js, css}"],
+  theme: {
+    fontFamily: {
+      'sans': [
+        'Inter',
+        'sans-serif'
+
+      ],
+      
+      'mono': [
+        'JetBrains Mono',
+        'monospace'
+      ],
+
+      'icon': [
+        'Material Symbols Outlined'
+      ]
+    },
+   
+    extend: {
+      colors: {
+        'mango': '#f3ae23'
+      }
+    },
+    animation: {
+      fade: 'fadeIn 1.2s ease-in-out',
+    },
+
+    keyframes: {
+      fadeIn: {
+        from: { opacity: 0 },
+        to: { opacity: 1 },
+      },
+    },
+  },
+  plugins: [],
+}
+
